4d4ad83a4659aeed5ae279a8f1f5db6d243a8dd3
NiiloOikari <Niilo.Oikari@gmail.com>
2024-09-06 12:57:51 +0300

    born

---
+++ data.js
@@ -0,0 +1,355 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-mntyl-ja-opisto",
+      "name": "Mäntylä ja opisto",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.9768559855360284,
+        "pitch": -0.11826670589761434,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.968729354399482,
+          "pitch": 0.0652206225108678,
+          "rotation": 0,
+          "target": "1-mntyln-edusta"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6511947100998974,
+          "pitch": 0,
+          "title": "Karstulan evankelinen opisto",
+          "text": "Päärakennus"
+        },
+        {
+          "yaw": -2.776389141636006,
+          "pitch": -0.04028391183475222,
+          "title": "Mäntylä",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-mntyln-edusta",
+      "name": "Mäntylän edusta",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.1193283774216862,
+        "pitch": -0.019036952098650417,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1892235312783015,
+          "pitch": 0.06310140572121803,
+          "rotation": 0,
+          "target": "2-aula"
+        },
+        {
+          "yaw": -2.310209792014591,
+          "pitch": 0.046356209218537,
+          "rotation": 0,
+          "target": "0-mntyl-ja-opisto"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-aula",
+      "name": "Aula",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.477519821882656,
+        "pitch": -0.009326306391995942,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8085241008806463,
+          "pitch": 0.12794991399412403,
+          "rotation": 0,
+          "target": "4-vasen-kytv"
+        },
+        {
+          "yaw": -0.8330913939932216,
+          "pitch": 0.2417080287381026,
+          "rotation": 0,
+          "target": "1-mntyln-edusta"
+        },
+        {
+          "yaw": 2.3303798193815313,
+          "pitch": 0.17231830379851232,
+          "rotation": 0,
+          "target": "3-yhteiset-tilat"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0382902548518818,
+          "pitch": 0.20342311987881345,
+          "title": "Lisää huoneita",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-yhteiset-tilat",
+      "name": "Yhteiset tilat",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.453532046690034,
+        "pitch": 0.012444800557007696,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0899559415238826,
+          "pitch": 0.02649461144168974,
+          "rotation": 0,
+          "target": "2-aula"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-vasen-kytv",
+      "name": "Vasen käytävä",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.2866333115725457,
+        "pitch": 0.045992607849090206,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7962623272148335,
+          "pitch": 0.2651946988135503,
+          "rotation": 0,
+          "target": "5-huone-nro-6"
+        },
+        {
+          "yaw": 2.0500065024043668,
+          "pitch": 0.3408021001158765,
+          "rotation": 0,
+          "target": "6-huone-nro-7"
+        },
+        {
+          "yaw": 3.117965953464786,
+          "pitch": 0.11228652533602279,
+          "rotation": 0,
+          "target": "2-aula"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-huone-nro-6",
+      "name": "Huone nro 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.0685567216796699,
+        "pitch": 0.3926459934488129,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8925768505487657,
+          "pitch": 0.3583788524184044,
+          "rotation": 0,
+          "target": "4-vasen-kytv"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-huone-nro-7",
+      "name": "Huone nro 7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.7017649517345834,
+        "pitch": 0.27609370201222916,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4654999891782126,
+          "pitch": 0.20347611174595492,
+          "rotation": 0,
+          "target": "4-vasen-kytv"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
